9fff0a4fe19aab8d23886138aa6cc8010e359eca
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-04-16 15:39:20 +0700

    membuat void binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -10,7 +10,7 @@ void input()
     while (true)
     {
         cout << "masukkan banyaknya elemen pada array(maksimal 10): ";
-        cin >> nPanjang;
+        cin >> npanjang ;
 
         if (npanjang<=10)
         {
@@ -42,7 +42,7 @@ void BubbleSortArray()
         {
             if (element [j] > element[j + 1])
             {
-                int temp = element[j;
+                int temp = element[j];
                 element [j] = element [j + 1];
                 element [j+1] = temp;
             }
@@ -67,5 +67,49 @@ void display()
     }
     cout << endl;
 }
+
+void BinarySearch()
+{
+    char ulang;
+    do 
+    {
+        cout << "\n===============================================\n";
+        cout << "      pencarian binary search                    \n";
+        cout << "===============================================\n";
+
+        cout << "maukkan element yang dicari : ";
+        cin >> x;
+
+        int low = 0;
+        int high = npanjang - 1;
+        do
+        {
+            int mid = (low + high ) / 2;
+
+            if (element[mid]== x)
+            {
+                cout << "[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+             if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+
+        }while ( low <= high);
+         if (low > high)
+        {
+        cout << "\n(x) Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n) : ";
+        cin >> ulang;
+    }while (ulang == 'y' || ulang == 'Y');
+}
+
 
 
