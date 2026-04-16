9d176a92a71e5a88bfb6eceec91a9d9595d588fe
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-04-16 15:12:09 +0700

    membuat pengulangan for while

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -35,7 +35,20 @@ void input()
 
 void BubbleSortArray()
 {
-    
+    int pass = 1;
+    do 
+    {
+        for (int j = 0;j <= npanjang - 1 - pass;j++)
+        {
+            if (element [j] > element[j + 1])
+            {
+                int temp = element[j;
+                element [j] = element [j + 1];
+                element [j+1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= npanjang - 1);
 }
 
 
