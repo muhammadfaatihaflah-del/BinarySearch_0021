68c3b5a7baffca0e69d369773ae2a46fe50d41f6
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-04-16 14:59:15 +0700

    membuat fungsi perulangan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 int element[10];
-int panjang;
+int npanjang;
 int x;
 
 void input()
 {
-    
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array(maksimal 10): ";
+        cin >> nPanjang;
+
+        if (npanjang<=10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. silahkan coba lagi.\n"
+        }
+    }
 }
 
